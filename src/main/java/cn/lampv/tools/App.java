32e1ff3c3614419214c2bbc79314bb0d691e0ff3
02166 <zzr@msdi.cn>
2019-03-27 16:13:59 +0800

    feat: 日志匹配Gitea默认日志文件夹

--- src/main/java/cn/lampv/tools/App.java
+++ src/main/java/cn/lampv/tools/App.java
@@ -1,21 +1,25 @@
 package cn.lampv.tools;
 
 import cn.lampv.tools.gitea.GiteaMaster;
-import jodd.datetime.JDateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Hello world!
+ * 启动程序
  */
 public class App {
     private static Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
+        System.out.println("Gitea Update: start....");
         try {
             GiteaMaster.GetInstance().Update();
+            System.out.println(" finished.");
         } catch (Exception exc) {
+            System.err.println(" error: " + exc.getMessage());
             logger.error("Gitea Update failed", exc);
+        } finally {
+            System.out.println(" exit.");
         }
     }
 }
